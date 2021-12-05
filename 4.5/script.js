45e337a7eec115a2e5459ae8f7ae815bf1741973
kylgry <kylgry@pm.me>
2021-12-05 14:51:03 -0700

    Add files via upload

--- 4.5/script.js
+++ 4.5/script.js
@@ -1,114 +1,118 @@
-const gameContainer = document.getElementById("game");
-const COLORS = [
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple"
-];
 
-// here is a helper function to shuffle an array
-// it returns the same array with values shuffled
-// it is based on an algorithm called Fisher Yates if you want to research more
-function shuffle(array) {
+let deckSize = 20;
+let deckColors = Array(deckSize);
+let deckState = Array(deckSize);
+let wait = 0;
+let complete = 0;
+let attempts = 0;
+
+function randomColors(array) {
+  for (let i = 0; i < array.length; i=i+2) {
+    array[i] = "#" + Math.floor(Math.random()*16777215).toString(16);
+    array[i+1] = array[i];
+  }
+}
+
+function shuffleDeck(array) {
   let counter = array.length;
-
-  // While there are elements in the array
   while (counter > 0) {
-    // Pick a random index
     let index = Math.floor(Math.random() * counter);
-
-    // Decrease counter by 1
     counter--;
-
-    // And swap the last element with it
     let temp = array[counter];
     array[counter] = array[index];
     array[index] = temp;
   }
-
   return array;
 }
 
-let shuffledColors = shuffle(COLORS);
-
-
-// this function loops over the array of colors
-// it creates a new div and gives it a class with the value of the color
-// it also adds an event listener for a click for each card
-function createDivsForColors(colorArray) {
-  for (let color of colorArray) {
-    // create a new div
+function createDivs(size) {
+  for (let i = 0; i < size; i++) {
     const newDiv = document.createElement("div");
-
-    // give it a class attribute for the value we are looping over
-    newDiv.classList.add(color);
-
-    // call a function handleCardClick when a div is clicked on
-    newDiv.addEventListener("click", handleCardClick);
-
-    // append the div to the element with an id of game
-    gameContainer.append(newDiv);
+    newDiv.id = i;
+    newDiv.addEventListener("click", cardClick);
+    document.getElementById("game").append(newDiv);
   }
 }
 
-// TODO: Implement this function!
-function handleCardClick(event) {
-  let clickedDiv = event.target;
+function refreshDisplay(){
+  for (let i = 0; i < deckState.length; i++) {
+    if (deckState[i] != "down"){
+      document.getElementById(i).style.backgroundColor = deckColors[i];
+    }
+    else {
+      document.getElementById(i).style.backgroundColor = "lightgray";
+    }
+  }
+  document.getElementById("score").innerHTML = `${attempts} attempts`;
+}
 
-  // do nothing if user clicks on card that is already face up
-  if (clickedDiv.classList[1] != null) {
-      return 
+function cardClick(event){
+  
+  clickedCard = event.target.id;
+
+  // if clicked card is not down, or wait is true, do nothing
+  if (deckState[clickedCard] != "down"){return};
+  if (wait == 1){console.log("wait!");return};
+
+  // make sure only one or no cards are up
+  let ups = 0;
+  for (state of deckState){if (state == "up") {ups++}};
+  if (ups > 1){console.log("you suck!")};
+  
+  // if no card was up, turn it up and wait for next click
+  if (deckState.includes("up") == false) {
+    deckState[clickedCard] = "up";
+    console.log(deckState);
+    refreshDisplay();
   }
 
-  // later logic insures zero or one cards can be up at a given time
-  // in either case card color should be shown
-  clickedDiv.style.backgroundColor = clickedDiv.classList[0];
-
-  // if there were no other cards "up" (excludes matched) then mark as up
-  if (document.querySelector(".up") == null) {
-      clickedDiv.classList.add("up");
-  }
-
-  // otherwise check to see if other up card is same color as the card user just clicked on
+  // if one card was already up, see if they match, otherwise set a timeout to turn both back down
   else {
-    //   score++;
-      const otherUp = document.querySelector(".up"); 
-      if (clickedDiv.classList[0] == otherUp.classList[0]) {
-          clickedDiv.classList.add("found");
-          otherUp.classList.remove("up");
-          otherUp.classList.add("found");
-      }
-      
-      // if they aren't the same then flip them back over
-      else {
-          otherUp.classList.remove("up");
-          
-          // need to capture the two elements into new variables in case user clicks
-          // faster than one second. then set timeout to turn them back over 
-          const flipOne = otherUp;
-          const flipTwo = clickedDiv; 
-          setTimeout(function (){
-            flipOne.style.backgroundColor = "grey";
-            flipTwo.style.backgroundColor = "grey";
-          },1000);
-      }
-  }
+    attempts++;
+    otherUp = deckState.indexOf("up");
+    deckState[clickedCard] = "up";
+    console.log(deckState);
+    refreshDisplay();
+    if (deckColors[otherUp] == deckColors[clickedCard]){
+      deckState[otherUp] = "matched";
+      deckState[clickedCard] = "matched";
+      console.log(deckState);
 
+      // check for win
+      if (!deckState.includes("down")) {
+        complete=1;
+        document.getElementById("status").innerHTML = "complete!";
+      }
+    }
+    else {
+      wait = 1;
+      firstCard = otherUp;
+      secondCard = clickedCard;
+      setTimeout(function (){
+        wait = 0
+      },1500);
+      setTimeout(function (){    
+        deckState[firstCard] = "down";
+        deckState[secondCard] = "down";
+        console.log(deckState);
+        refreshDisplay();
+      },1000);
+    }
+  }
 }
 
-// when the DOM loads
-createDivsForColors(shuffledColors);
+function startGame(){
+  randomColors(deckColors);
+  shuffleDeck(deckColors);
+  deckState.fill("down");
+  console.log(deckState);
+  attempts = 0;
+  complete = 0;
+  refreshDisplay();
+  document.getElementById("status").innerHTML = "play!";
+}
 
-// start score
-// let score = 0;
-// const newDiv = document.createElement("div");
-// document.body.append(newDiv);
-
-
+document.getElementById("reset").addEventListener("click", startGame);
+createDivs(deckSize);
+startGame();
 
