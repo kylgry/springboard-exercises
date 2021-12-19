fe738b224030cec0bbc818e1bd7be79bff7af786
Kyle Gray <kylgry@pm.me>
2021-12-19 13:13:45 -0700

    10.3 arrow functions

---
+++ 10.3_arrow-functions/script.js
@@ -0,0 +1,26 @@
+// function double(arr) {
+//   return arr.map(function(val) {
+//     return val * 2;
+//   });
+// }
+//
+// function squareAndFindEvens(numbers){
+//   var squares = numbers.map(function(num){
+//     return num ** 2;
+//   });
+//   var evens = squares.filter(function(square){
+//     return square % 2 === 0;
+//   });
+//   return evens;
+// }
+
+const double = (arr) => arr.map((val) => val*2);
+
+const squareAndFindEvens = (numbers) => {
+  let squares = numbers.map((num) => num ** 2);
+  let evens = squares.filter((square) => square % 2 === 0);
+  return evens;
+}
+
+console.log(double([1,2,3,4,5]));
+console.log(squareAndFindEvens([1,2,3,4,5]));
