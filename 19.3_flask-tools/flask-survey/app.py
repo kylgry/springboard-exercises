210f375a7e6e0cb05d810dd054a01b85f1f6532e
Kyle Gray <kylgry@pm.me>
2022-02-08 18:45:28 -0700

    19.3 flask tools

---
+++ 19.3_flask-tools/flask-survey/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, render_template, redirect, flash
+import surveys
+
+app = Flask(__name__)
+
+responses = []
+
+@app.route('/')
+def display_root():
+    title = surveys.satisfaction_survey.title
+    instructions = surveys.satisfaction_survey.instructions
+    return render_template('index.html', title=title, instructions=instructions)
+
+
+# i think logically there is no need to have the question number in the url
+# this route can just always direct to the next unanswered question by
+# looking at length of response array. simpler than adding logic in response to
+# url is correct or not. step 7 becoems irrelevant since user can't visit wrong url.
+
+@app.route('/questions')
+def display_questions():
+    n = len(responses)
+    if n == len(surveys.satisfaction_survey.questions):
+        return redirect('/thankyou')
+    else:
+        question = surveys.satisfaction_survey.questions[n].question
+        choices = surveys.satisfaction_survey.questions[n].choices
+        return render_template('question.html', n=n, question=question, choices=choices)
+
+@app.route('/answer', methods=['POST'])
+def handle_answer():
+    responses.append(request.form.getlist('options')[0])
+    return redirect('/questions')
+
+@app.route('/thankyou')
+def display_thankyou():
+    return render_template('thankyou.html')
