341536454ba11ccabdaed57d3b0287c41a29008d
Kyle Gray <kylgry@pm.me>
2022-06-18 10:19:16 -0600

    39.3 pokedex fixed key warning

--- 39.3_create-react-app/pokedex/src/Pokedex.js
+++ 39.3_create-react-app/pokedex/src/Pokedex.js
@@ -6,7 +6,7 @@ function Pokedex({pokemon = pokemon_default}) {
   return (
     <div>
       <h2>Pokedex</h2>
-      {pokemon.map(p => <Pokecard pokemon={p} />)}
+      {pokemon.map(p => <Pokecard key={p.id} pokemon={p} />)}
     </div>
   )
 }
